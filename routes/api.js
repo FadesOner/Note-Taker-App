0cd3aa8d9d9dc3915dbd5a478a1e0a1c4322f7d5
Luis Debourg <Luisdebourgm@gmail.com>
2022-05-12 16:00:12 +1000

    POST and DELETE added

--- routes/api.js
+++ routes/api.js
@@ -3,58 +3,35 @@ const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const fs = require ('fs');
 
-
 router.get('/notes',(req, res) => {
 res.sendFile(path.join(__dirname, '..', 'db', 'db.json'))
 }
  );
-
+// this will post the new notes on the left side of the website also will add the id number to each note
 router.post('/notes', (req, res) => {
 const newNote = {
     title: req.body.title,
     text: req.body.text,
-    id: uuid.v4()
+    id: uuidv4()
 }
 const savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'db', 'db.json'), 'utf8'))
 savedNotes.push(newNote)
 
 fs.writeFileSync(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(savedNotes), 'utf8')
 res.json(newNote)
-    
-//    let savedNotes = JSON.parse(fs.readFileSync(path.join (__dirname, '..', 'db', 'db.json'), "utf8"));
-//     let newNote = req.body;
+});
 
+// this will delete the notes 
 
-//    let uniqueId = (savedNotes.length).toString();
-//     newNote.id = uniqueId;
-//     savedNotes.push(newNote);
-
-
-//     fs.writeFileSync('..', 'db', 'db.json', JSON.stringify(savedNotes));
-
-//     res.json(savedNotes);
- });
-
- // router.delete('');
- router.delete('/api/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
      
-    // var savedNotes = JSON.parse(fs.readFileSync(path.join (__dirname, '..', 'db', 'db.json'), "utf8"));
-    // var noteID = req.params.id;
-    // var newID = 0;
+const savedNotes = JSON.parse(fs.readFileSync(path.join (__dirname, '..', 'db', 'db.json'), "utf8"));
+const delNotes = savedNotes.filter(note => note.id !== req.params.id)
 
-    // savedNotes = savedNotes.filter(currentNote => {
+fs.writeFileSync(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(delNotes), 'utf8');
+res.json(delNotes);
 
-    //     return currentNote.id != noteID;
-    // })
-
-    // for (currentNote of savedNotes) {
-    //     currentNote.id = newID.toString();
-    //     newID++;
-    // }
-
-    // fs.writeFileSync('..', 'db', 'db.json', JSON.stringify(savedNotes));
-    // return res.json(savedNotes);
-  });
+});
   
 
 module.exports = router;
